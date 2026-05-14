551c6aa31eed0622befb29bae9c2e203194e0fb5
flbdennis <flbdennis@163.com>
2026-05-14 23:49:33 +0800

    修复bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   // 网站 URL，部署时改为最终域名
-  site: 'https://esay-tools.pages.dev',
+  site: 'https://metis-tools.pages.dev',
   // 输出静态 HTML
   output: 'static',
   // URL 末尾不添加斜杠
